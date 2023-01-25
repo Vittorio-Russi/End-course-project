e532c4a27edd1ce942262464f69401ca64ce4f6b
Vittorio-Russi <123548473+Vittorio-Russi@users.noreply.github.com>
2023-01-25 11:56:21 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+import PROJECT_CODE as pc
+import unittest
+import pickle as pk
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pylab as plt
